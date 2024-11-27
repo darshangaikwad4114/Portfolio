ad56ec33e96c7e3362b223082a77c1131e181105
darshan <darshangaikwad4114@gmail.com>
2024-11-27 19:25:39 +0530

    refactor: initial changes

--- src/js/script.js
+++ src/js/script.js
@@ -5,25 +5,25 @@
   $('a.js-scroll-trigger[href*="#"]')
     .not('[href="#"]')
     .click(function () {
-    if (
-      location.pathname.replace(/^\//, '') ==
+      if (
+        location.pathname.replace(/^\//, '') ==
         this.pathname.replace(/^\//, '') &&
-      location.hostname == this.hostname
-    ) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate(
-          {
-            scrollTop: target.offset().top,
-          },
-          100,
-          'easeInOutExpo'
-        );
-        return false;
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html, body').animate(
+            {
+              scrollTop: target.offset().top,
+            },
+            100,
+            'easeInOutExpo'
+          );
+          return false;
+        }
       }
-    }
-  });
+    });
 
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function () {
