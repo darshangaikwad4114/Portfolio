62798886eef2d7178327e484497fa6bb49c0e090
darshan <darshangaikwad4114@gmail.com>
2024-12-19 00:03:35 +0530

    Replace node-sass with sass

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,26 @@
 (function ($) {
-  'use strict'; // Start of use strict
+  "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]')
     .not('[href="#"]')
     .click(function () {
       if (
-        location.pathname.replace(/^\//, '') ==
-        this.pathname.replace(/^\//, '') &&
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
         location.hostname == this.hostname
       ) {
         var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
         if (target.length) {
-          $('html, body').animate(
+          $("html, body").animate(
             {
               scrollTop: target.offset().top,
             },
             100,
-            'easeInOutExpo'
+            "easeInOutExpo"
           );
           return false;
         }
@@ -26,12 +28,12 @@
     });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function () {
-    $('.navbar-collapse').collapse('hide');
+  $(".js-scroll-trigger").click(function () {
+    $(".navbar-collapse").collapse("hide");
   });
 
   // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#sideNav',
+  $("body").scrollspy({
+    target: "#sideNav",
   });
 })(jQuery); // End of use strict
